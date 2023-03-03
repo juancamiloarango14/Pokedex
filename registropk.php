9034854ddd9748fe5630f07df67a4d7bfc910bf2
juancamiloarango14 <100304866+juancamiloarango14@users.noreply.github.com>
2023-03-03 17:00:34 -0500

    creacion de clases, front y back

---
+++ registropk.php
@@ -0,0 +1,18 @@
+<?php
+include("template/cabecera.php");
+?>
+
+<div class="jumbotron text-center">
+    <h1 class="display-3">Bienvenido a la pokedex</h1>
+    <p class="lead">Aqui puedes registrar y consultar los distintos pokemones...</p>
+    <hr class="my-2">
+
+    <p>Mas Informacion...</p>
+    <p class="lead">  
+        <a class="btn btn-dark" href="#" role="button">Comencemos...</a>
+    </p>
+</div>
+
+<?php
+include("template/pie.php");
+?>
