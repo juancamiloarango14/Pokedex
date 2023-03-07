9d99f74909c23b9c08031996ac1251dc1e96d006
juancamiloarango14 <100304866+juancamiloarango14@users.noreply.github.com>
2023-03-07 17:40:41 -0500

    avance

--- admin/crudPk.php
+++ admin/crudPk.php
@@ -2,6 +2,24 @@
 include("template/cabecera.php");
 ?>
 
+<?php
+
+ if (isset($_POST['submit'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $type = $_POST['type'];
+    $image_url = $_POST['image_url'];
+  
+    $sql = "INSERT INTO pokemon (name, type, image_url, created_at, updated_at) VALUES (:name, :type, :image_url, NOW(), NOW())";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['name' => $name, 'type' => $type, 'description' => $description, 'image_url' => $image_url]);
+  }
+
+  
+  include("config/bd.php");
+  
+
+?>
 
 <div class="col-md-5">    
 
@@ -15,22 +33,22 @@ include("template/cabecera.php");
 
 <div class = "form-group">
 <label for="ID">ID:</label>
-<input type="text" required readonly class="form-control"  name="txtID" id="txtID"  placeholder="ID">
+<input type="text" required readonly class="form-control"   name="id" id="id"  placeholder="ID">
 </div>
 
 <div class = "form-group">
 <label for="Nombre">Name Pokemon:</label>
-<input type="text" required class="form-control" name="txtNombre" id="txtNombre"  placeholder="Nombre del Pokemon">
+<input type="text" required class="form-control"name="name" id="name"  placeholder="Nombre del Pokemon">
 </div>
 
 <div class = "form-group">
 <label for="Nombre">Type Pokemon:</label>
-<input type="text" required class="form-control" name="txtNombre" id="txtNombre"  placeholder="Tipo del Pokemon">
+<input type="text" required class="form-control"  name="type" id="type"  placeholder="Tipo del Pokemon">
 </div>
 
 <div class = "form-group">
 <label for="txtNombre">Imagen:</label>
-<input type="file" class="form-control" name="txtImagen" id="txtImagen" placeholder="foto del pokemon">
+<input type="file" class="form-control" value=" name="image_url" id="image_url" placeholder="foto del pokemon">
 </div>
 
 <div class="btn-group" role="group" aria-label="">
@@ -70,14 +88,15 @@ include("template/cabecera.php");
         
            </td>
 
-            <td>
-                <form method="post">
+            <td> 
+            <form method="POST">
 
-                <input type="hidden" name="txtID" id="" value=" " />
+            <input type="hidden" name="txtID" id="" value="<?php echo $pokemon['id'];?>" />
 
-                <input type="submit" name="accion" value="Seleccionar" class="btn btn-light"/>
+            <input type="submit" name="accion" value="Seleccionar" class="btn btn-primary"/>
 
-                <input type="submit" name="accion" value="Borrar" class="btn btn-dark"/>
+            <input type="submit" name="accion" value="Borrar" class="btn btn-danger"/>
+
 
                 </form>
             
